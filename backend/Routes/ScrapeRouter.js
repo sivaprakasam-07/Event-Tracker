996cab2d61969b1ccb43b7c72670eb387ce09d4e
sivaprakasam-07 <tamilmanisivaprakasam5@gmail.com>
2025-06-12 16:48:06 +0530

    scrapping done in unstop also

--- backend/Routes/ScrapeRouter.js
+++ backend/Routes/ScrapeRouter.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {
-    scrapeDevpost,
-    getScrapedEvents,
-} = require("../Controller/scrapeController");
+const { scrapeDevpost, scrapeUnstop, getScrapedEvents, } = require("../Controller/scrapeController");
 
 router.get("/scrape-devpost", scrapeDevpost);
+router.get("/scrape-unstop", scrapeUnstop);
 router.get("/scraped-events", getScrapedEvents);
 
 module.exports = router;
